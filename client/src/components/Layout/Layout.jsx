8991151f86e5f13bed4c45e06ac2c8466e73550f
Nilesh Kurrey <nileshkurrey1@gmail.com>
2023-08-02 14:20:58 +0530

    changes in audience link

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -27,7 +27,7 @@ const Layout = () => {
 
       const res = await getAccessTokenWithPopup({
         authorizationParams: {
-          audience: "http://localhost:8000/",
+          audience: "https://property-mangement-app.vercel.app/",
           scope: "openid profile email",
         },
       });
